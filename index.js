15fc94ad0c30506949e716cace06beeb9be02a0e
CR0416 <karinachen1314@yahoo.com>
2022-03-22 00:40:37 -0400

    initial

---
+++ index.js
@@ -0,0 +1,22 @@
+const port = 3000,
+http = require("http"),
+fs = require('fs');
+
+httpStatus= require("http-status-codes"),
+fs.readFile('./index.html', function(err, html) {
+    if (err){
+        throw err;    }
+    app= http.createServer((request, response) => {
+    console.log("Received an incoming request!");                                                                                                                                                                                           
+    response.writeHead(httpStatus.StatusCodes.OK, {                                                                                                                                                                                         
+        "Content-Type": "text/html"                                                                                                                                                                                                         
+    })                                                                                                                                                                                                                                      
+    response.write(html);     
+    response.end()                                                                                                                                                                                
+})
+});                                                                                                                                                                                                                                     
+
+app.listen(port)                                                                                                                                                                                                                           
+console.log(                                                                                                                                                                                                                                
+    `the server has started and is litening on port number: =${port}`                                                                                                                                                                       
+);                                                                          
